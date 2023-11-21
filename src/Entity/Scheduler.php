59f448b3211434c4170c4b321633ccf178290784
Greg LAMY <g.lamy@loligo.io>
2023-11-21 12:48:18 +0100

    Update Copyrights

--- src/Entity/Scheduler.php
+++ src/Entity/Scheduler.php
@@ -1,17 +1,14 @@
 <?php
 
 /**
- * Copyright (c) 2023 Greg LAMY <greg@bytespin.net>
- *
- * This project is hosted on GitHub at:
+ * This file is part of the ByteSpin/ConsoleCommandSchedulerBundle project.
+ * The project is hosted on GitHub at:
  *  https://github.com/ByteSpin/ConsoleCommandSchedulerBundle.git
  *
- * This bundle was originally developed as part of an ETL project.
+ * Copyright (c) Greg LAMY <greg@bytespin.net>
  *
- * ByteSpin/ConsoleCommandSchedulerBundle is a Symfony 6.3 simple bundle that allows you to schedule console commands easily:
- * - Use the latest messenger/scheduler Symfony 6.3+ components,
- * - Log all console commands data (last execution time, duration, return code) in database and log file,
- * - An admin interface is available with the help of EasyCorp/EasyAdmin bundle
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace ByteSpin\ConsoleCommandSchedulerBundle\Entity;
