f8eb7fc990bcc54f34b540f7c40961f3d37d4ae1
Aurélien HANTRAYE <aurelien.hantraye@flashmer.com>
2025-06-06 12:57:50 +0200

    wip

--- src/Provider/MessengerQueueProvider.php
+++ src/Provider/MessengerQueueProvider.php
@@ -14,34 +14,23 @@
 namespace ByteSpin\ConsoleCommandSchedulerBundle\Provider;
 
 use AllowDynamicProperties;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\Yaml\Yaml;
+use Symfony\Contracts\Service\ServiceCollectionInterface;
 
 #[AllowDynamicProperties] class MessengerQueueProvider
 {
-
     public function __construct(
-        private readonly string $projectDir,
+        private readonly ServiceCollectionInterface $locator,
     ) {
     }
 
     public function listMessengerQueues(): array
     {
-        $file = $this->projectDir . '/config/packages/messenger.yaml';
-
-        if (!file_exists($file)) {
-            return [];
-        }
-
         $transportNames = [];
-        try {
-            $config = Yaml::parseFile($file);
-            foreach ($config['framework']['messenger']['transports'] ?? [] as $name => $transport) {
-                $transportNames[$name] = $name;
+
+        foreach ($this->locator as $serviceId => $service) {
+            if (!str_contains($serviceId, 'messenger.')) {
+                $transportNames[$serviceId] = $serviceId;
             }
-        } catch (\Exception $e) {
-            return [];
         }
 
         return $transportNames;
